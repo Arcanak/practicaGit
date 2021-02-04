893ef228a302d8988139d8977446a8b19137c17d
Diurno <Diurno@C8PC27>
2021-02-04 13:04:34 +0100

    Es un hola mundo básico

---
+++ holaMundo/src/main/java/com/mycompany/holamundo/HolaMundo.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.holamundo;
+
+/**
+ *
+ * @author Diurno
+ */
+public class HolaMundo {
+    public static void Main(String[] args){
+        System.err.println("Hola mundo");
+    }
+}
