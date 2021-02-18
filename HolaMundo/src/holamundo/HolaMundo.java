a5afbb22391f4577c72c51cbf6cec4893b731dd6
Diurno <Diurno@C8PC23.informatica.comercio>
2021-02-18 11:26:13 +0100

    Version 3

---
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package holamundo;
+
+/**
+ *
+ * @author Diurno
+ */
+public class HolaMundo {
+    private static int misHuevos = 2;
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola Puta");
+    }
+    
+    public int getMisHuevos()
+    {
+        return misHuevos;
+    }
+    
+}
